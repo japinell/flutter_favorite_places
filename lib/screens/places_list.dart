53c5557c08f7c31365df1716192acda4067e5bdd
Jose A. Pinell <japinell@yahoo.com>
2025-09-01 13:18:06 -0700

    Added scaffold  code for PlacesList screen

---
+++ lib/screens/places_list.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Favorite Places"),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+      body: ...,
+    );
+  }
+}
