32ea0c08996fedb4b6469f2d33f717d289d8bb0d
Jose A. Pinell <japinell@yahoo.com>
2025-09-01 13:28:54 -0700

    Added PlacesList widget

---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+import "package:flutter_favorite_places/models/place.dart";
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return const Center(child: Text("No favorite places added yet."));
+    }
+
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, idx) => ListTile(
+        title: Text(
+          places[idx].title,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ),
+    );
+  }
+}
