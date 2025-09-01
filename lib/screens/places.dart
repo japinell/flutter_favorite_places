20bf70eadfa78ae343e03f34128e471d04cd93f5
Jose A. Pinell <japinell@yahoo.com>
2025-09-01 15:01:15 -0700

    Wired Places screen to Places provider

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,17 @@
 import "package:flutter/material.dart";
+import "package:flutter_favorite_places/providers/places.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
 import "package:flutter_favorite_places/widgets/places_list.dart";
 import "package:flutter_favorite_places/screens/add_places.dart";
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final places = ref.watch(placesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Favorite Places"),
@@ -21,7 +26,7 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: PlacesList(places: []),
+      body: PlacesList(places: places),
     );
   }
 }
