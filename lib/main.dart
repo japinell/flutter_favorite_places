bc41c78c37554daf14be00914102587783908ad4
Jose A. Pinell <japinell@yahoo.com>
2025-09-01 14:34:53 -0700

    Enabled flutter_riverpod provider throughout the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "package:flutter_favorite_places/screens/places.dart";
 
@@ -19,7 +20,7 @@ final theme = ThemeData().copyWith(
   ),
 );
 
-void main() => runApp(const App());
+void main() => runApp(ProviderScope(child: const App()));
 
 class App extends StatelessWidget {
   const App({super.key});
