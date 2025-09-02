7a341f6dc5102f647cbcdeabf71778e99451f396
Jose A. Pinell <japinell@yahoo.com>
2025-09-01 20:09:47 -0700

    Added PlaceLocation class to Place model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -7,6 +7,20 @@ class Place {
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 
-  Place({required this.title, required this.image}) : id = uuid.v4();
+  Place({required this.title, required this.image, required this.location})
+    : id = uuid.v4();
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
 }
