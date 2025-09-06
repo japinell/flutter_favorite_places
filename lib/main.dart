dfc6731ea9b9430de08f8c09826b8c3992edaccd
Jose A. Pinell <japinell@yahoo.com>
2025-09-06 15:17:17 -0700

    Added logic to main to initialize the map renderer when running on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import "dart:async";
+
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
+
+import "package:google_maps_flutter_android/google_maps_flutter_android.dart";
+import "package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart";
 
 import "package:flutter_favorite_places/screens/places.dart";
 
@@ -21,9 +26,48 @@ final theme = ThemeData().copyWith(
   ),
 );
 
+Completer<AndroidMapRenderer?>? androidMapRendererCompleter;
+
 void main() async {
   await dotenv.load(fileName: ".env");
+
+  final GoogleMapsFlutterPlatform mapsImplementation =
+      GoogleMapsFlutterPlatform.instance;
+
+  if (mapsImplementation is GoogleMapsFlutterAndroid) {
+    await initializeAndroidMapRenderer();
+  }
+
   runApp(ProviderScope(child: const App()));
+}
+
+Future<AndroidMapRenderer?> initializeAndroidMapRenderer() async {
+  if (androidMapRendererCompleter != null) {
+    return androidMapRendererCompleter!.future;
+  }
+
+  final Completer<AndroidMapRenderer?> completer =
+      Completer<AndroidMapRenderer?>();
+  androidMapRendererCompleter = completer;
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final GoogleMapsFlutterPlatform mapsImplementation =
+      GoogleMapsFlutterPlatform.instance;
+  if (mapsImplementation is GoogleMapsFlutterAndroid) {
+    unawaited(
+      mapsImplementation
+          .initializeWithRenderer(AndroidMapRenderer.latest)
+          .then(
+            (AndroidMapRenderer initializedRenderer) =>
+                completer.complete(initializedRenderer),
+          ),
+    );
+  } else {
+    completer.complete(null);
+  }
+
+  return completer.future;
 }
 
 class App extends StatelessWidget {
