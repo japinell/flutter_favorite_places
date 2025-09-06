641ee0396a2c65273cb803742bf415d7fb037335
Jose A. Pinell <japinell@yahoo.com>
2025-09-06 14:14:18 -0700

    Removed extra package import in the Map screen

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
 
 import "package:flutter_favorite_places/models/place.dart";
