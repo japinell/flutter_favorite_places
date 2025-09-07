5b4ed6f2c7c58f1cb3b8c00b190a35dc2beca54f
Jose A. Pinell <japinell@yahoo.com>
2025-09-06 18:25:31 -0700

    Added logic to PlacesNotifier provider to copy the picked image to the apps documents folder

--- lib/providers/places.dart
+++ lib/providers/places.dart
@@ -1,12 +1,23 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:path_provider/path_provider.dart" as syspath;
+import "package:path/path.dart" as path;
 
 import "package:flutter_favorite_places/models/place.dart";
 
 class PlacesNotifier extends StateNotifier<List<Place>> {
   PlacesNotifier() : super(const []);
 
-  void addPlace(Place place) {
-    state = [...state, place];
+  void addPlace(Place place) async {
+    final appPath = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(place.image.path);
+    final newImage = await place.image.copy("${appPath.path}/$fileName");
+    final newPlace = Place(
+      title: place.title,
+      image: newImage,
+      location: place.location,
+    );
+
+    state = [...state, newPlace];
   }
 }
 
