c717504f16ce8faf5912040465e5b814adb3e63a
Jose A. Pinell <japinell@yahoo.com>
2025-09-06 18:36:58 -0700

    Implemented the logic to insert the data to sqllite in PlacesNotifier provider

--- lib/providers/places.dart
+++ lib/providers/places.dart
@@ -1,6 +1,8 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:path_provider/path_provider.dart" as syspath;
 import "package:path/path.dart" as path;
+import "package:sqflite/sqflite.dart" as sql;
+import "package:sqflite/sqlite_api.dart";
 
 import "package:flutter_favorite_places/models/place.dart";
 
@@ -16,6 +18,26 @@ class PlacesNotifier extends StateNotifier<List<Place>> {
       image: newImage,
       location: place.location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final database = await sql.openDatabase(
+      path.join(dbPath, "places.db"),
+      onCreate: (db, version) {
+        db.execute(
+          "CREATE TABLE places(id TEXT PRIMARY KEY, title TEXT, image TEXT, latitude REAL, longitude REAL, address TEXT)",
+        );
+      },
+      version: 1,
+    );
+
+    database.insert("places", {
+      "id": newPlace.id,
+      "title": newPlace.title,
+      "image": newPlace.image.path,
+      "latitude": newPlace.location.latitude,
+      "longitude": newPlace.location.longitude,
+      "address": newPlace.location.address,
+    });
 
     state = [...state, newPlace];
   }
